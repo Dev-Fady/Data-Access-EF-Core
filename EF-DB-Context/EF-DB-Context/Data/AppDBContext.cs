2f008d5d519fab423c2d62ec4e10a4dfed459e2b
Dev-Fady <fadymobiledev@gmail.com>
2025-08-21 17:16:42 +0300

    type DbContext --- Internal --- External --- Dependency Injection ---  DBContext Factory --- Execute Concurrently --- DBContext Pooling

---
+++ EF-DB-Context/EF-DB-Context/Data/AppDBContext.cs
@@ -0,0 +1,38 @@
+﻿using EF_DB_Context.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_DB_Context.Data
+{
+    public class AppDBContext :DbContext
+    {
+        public DbSet<Wallet> wallets { get; set; } =null!;
+
+        #region DBContext External Configuration
+        public AppDBContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        #endregion
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            #region DBContext Internal Configuration
+            //var configuration = new ConfigurationBuilder()
+            // .AddJsonFile("appsettings.json")
+            // .Build();
+
+            //string connectionString = configuration.GetSection("constr").Value;
+
+            //optionsBuilder.UseSqlServer(connectionString);
+            #endregion
+        }
+    }
+}
---
+++ EF-DB-Context/EF-DB-Context/Program.cs
@@ -0,0 +1,170 @@
+﻿using EF_DB_Context.Data;
+using EF_DB_Context.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace EF_DB_Context
+{
+   
+    internal class Program
+    { 
+        //static AppDBContext context;
+        static async Task Main(string[] args)
+        {
+            #region DBContext External Configuration
+            //var configuration = new ConfigurationBuilder()
+            // .AddJsonFile("appsettings.json")
+            // .Build();
+
+            //string connectionString = configuration.GetSection("constr").Value;
+
+            //var optionsBuilder = new DbContextOptionsBuilder();
+
+            //optionsBuilder.UseSqlServer(connectionString);
+            //var options = optionsBuilder.Options;
+
+
+            //using (var context = new AppDBContext(options))
+            //{
+            //    foreach (var item in context.wallets)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion
+
+            #region Dependency Injection
+            //var configuration = new ConfigurationBuilder()
+            // .AddJsonFile("appsettings.json")
+            // .Build();
+
+            //string connectionString = configuration.GetSection("constr").Value!;
+
+            //var services = new ServiceCollection();
+
+            //services.AddDbContext<AppDBContext>(
+            //    options => options.UseSqlServer(connectionString)
+            //    );
+
+            //IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            //using (var context =serviceProvider.GetService<AppDBContext>())
+            //{
+            //    foreach (var item in context!.wallets)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion
+
+            #region DBContext Factory 
+            //var configuration = new ConfigurationBuilder()
+            // .AddJsonFile("appsettings.json")
+            // .Build();
+
+            //string connectionString = configuration.GetSection("constr").Value!;
+
+            //var services = new ServiceCollection();
+
+            //services.AddDbContextFactory<AppDBContext>(
+            //    options => options.UseSqlServer(connectionString)
+            //    );
+
+            //IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            //var contextFactory = serviceProvider.GetService<IDbContextFactory<AppDBContext>>();
+            //using (var context = contextFactory!.CreateDbContext())
+            //{
+            //    foreach (var item in context!.wallets)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion
+
+            #region Execute Concurrently
+            //var configuration = new ConfigurationBuilder()
+            // .AddJsonFile("appsettings.json")
+            // .Build();
+
+            //string connectionString = configuration.GetSection("constr").Value!;
+
+            //var services = new ServiceCollection();
+
+            //services.AddDbContext<AppDBContext>(
+            //    options => options.UseSqlServer(connectionString)
+            //    );
+
+            //IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            //context=serviceProvider.GetRequiredService<AppDBContext>();
+
+            //var tasks = new[]
+            //{
+            //    Task.Factory.StartNew(()=>Job1()),
+            //    Task.Factory.StartNew(()=>Job2()),
+            //};
+            //await Task.WhenAll(tasks).ContinueWith(t =>
+            //{
+            //    Console.WriteLine("Jop1 & jop2 execute Concurrently");
+            //});
+
+            //using (var test= serviceProvider.GetService<AppDBContext>())
+            //{
+            //     foreach (var item in test!.wallets)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion
+
+            #region DBContext Pooling
+            //var configuration = new ConfigurationBuilder()
+            // .AddJsonFile("appsettings.json")
+            // .Build();
+
+            //string connectionString = configuration.GetSection("constr").Value!;
+
+            //var services = new ServiceCollection();
+
+            //services.AddDbContextPool<AppDBContext>(
+            //    options => options.UseSqlServer(connectionString)
+            //    );
+
+            //IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            //using (var context = serviceProvider.GetService<AppDBContext>())
+            //{
+            //    foreach (var item in context!.wallets)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion
+
+        }
+        //static async Task Job1()
+        //{
+        //    var w1 = new Wallet
+        //    {
+        //        Holder = "NaNa",
+        //        Balance = 900
+        //    };
+        //    context.wallets.Add(w1);
+        //    await context.SaveChangesAsync();
+        //}
+        //static async Task Job2() 
+        //{
+        //    var w2 = new Wallet
+        //    {
+        //        Holder = "SoSo",
+        //        Balance = 1999
+        //    };
+        //    context.wallets.Add(w2);
+        //    await context.SaveChangesAsync();
+        //}
+    }
+}
