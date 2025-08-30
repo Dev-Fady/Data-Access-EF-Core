79265a2cffcda98c56ae2983d755fb6acec58cbe
Dev-Fady <fadymobiledev@gmail.com>
2025-08-30 16:07:06 +0300

    Reverse Engineering

---
+++ ReverseEngineering/ReverseEngineering/Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ReverseEngineering.Entities;
+
+namespace ReverseEngineering.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Speaker> Speakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.;Database=TechTalk;Integrated Security=SSPI;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Events__3214EC07A1839933");
+
+            entity.HasOne(d => d.Speaker).WithMany(p => p.Events)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Events__SpeakerI__398D8EEE");
+        });
+
+        modelBuilder.Entity<Speaker>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Speakers__3214EC079C5F2C5C");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ReverseEngineering/ReverseEngineering/Program.cs
@@ -0,0 +1,55 @@
+﻿using ReverseEngineering.Data;
+using System;
+
+namespace ReverseEngineering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in new AppDbContext().Speakers)
+            {
+                Console.WriteLine(item.FirstName + " " + item.LastName);
+            }
+        }
+    }
+}
+
+#region Reverse Engineering using Package Manger Console (PMC)
+/*
+// Step #1: Package Manager Console (PMC)
+//    Tools -> Nuget Package Manager -> Package Manager Console
+
+// Step #2: Package Manager Console (PMC) Tool 
+//    Install-Package Microsoft.EntityFrameworkCore.Tools
+
+// Step #3: Install Nuget Page on Project Microsoft.EntityFrameworkCore.Design
+// Microsoft.EntityFrameworkCore.SqlServer
+
+// Step #4: Install Provider in the project Microsoft.EntityFrameworkCore.SqlServer
+
+// Step #5: Run Command (Full)
+//    Scaffold-DbContext '[Connection String]' [Provider]
+
+ scaffold-DbContext 'Data Source = . ; database = TechTalk ; Integrated Security = SSPI ; TrustServerCertificate = True' 
+    Microsoft.EntityFrameworkCore.SqlServer -context AppDbContext -ContextDir Data -OutputDir Entites
+*/
+#endregion
+
+#region Reverse Engineering NET CLI
+/*
+*         // Step #1: Windows Terminal (Command Prompt) 
+
+// Step #2: Install Ef-Core tool globally
+//    dotnet tool install --global dotnet-ef    (new)
+//    dotnet tool update --global dotnet-ef     (to upgrade)
+
+// Step #3: Install Provider in the project Microsoft.EntityFrameworkCore.SqlServer
+
+// Step #4: Run Command (Full)
+//    dotnet ef dbcontext scaffold '[Connection String]' [Provider]
+dotnet ef dbcontext scaffold "Data Source=.;Database=TechTalk;Integrated Security=SSPI;TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer --data-annotations --context AppDbContext --context-dir Data --output-dir Entities
+
+* 
+*/
+#endregion
